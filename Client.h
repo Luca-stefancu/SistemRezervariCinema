ae30dfdff1cfb5ce7b4b09a711dd9a9c33a3c442
Luca-stefancu <luca.stefancu@student.usv.ro>
2026-06-02 15:44:51 +0300

    Enhance Client class with tipClient attribute
    Added a new member 'tipClient' and updated the constructor and setter methods accordingly.

--- Client.h
+++ Client.h
@@ -9,10 +9,11 @@ private:
     string prenume;
     string email;
     string telefon;
+    string tipClient; // "Student", "Pensionar", "Membru fidel", "Normal"
 
 public:
     // Constructor
-    Client(int id, string nume, string prenume, string email, string telefon);
+    Client(int id, string nume, string prenume, string email, string telefon, string tipClient);
 
     // Getteri
     int getId() const;
@@ -20,12 +21,14 @@ public:
     string getPrenume() const;
     string getEmail() const;
     string getTelefon() const;
+    string getTipClient() const;
 
     // Setteri
     void setNume(string nume);
     void setPrenume(string prenume);
     void setEmail(string email);
     void setTelefon(string telefon);
+    void setTipClient(string tipClient);
 
     // Afisare
     void afisare() const;
